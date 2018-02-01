ae72a7cd9e5bbe602422b55ede984eedd1e699dc
Emily Palmer <empalmer@reed.edu>
2018-02-01 12:45:26 -0800

    wk2 edits

--- museR/R/read.R
+++ museR/R/read.R
@@ -1,5 +1,5 @@
 
-
+library(stringr)
 ### kern2df ####
 
 #' Takes a .krn spline and converts into data frame
@@ -12,33 +12,22 @@
 kern2df <- function(spline){ # takes a spline(".krn") as input
   data <- readLines(spline)
   data <- data[-grep("!!linebreak", data)] #removes all linebreak text
-  staff <- grep("staff[0-9]", data, value = T) # which staff are we talking about?
-  key <- grep( "\\[[A-z]"  ,data, value = T) # what is the key sig of the piece?
-  data <- data[-grep("\\*", data)]
+  data <- data[-grep("\\*", data)] # removes ... 
 
-  measures <- grep( "="   ,data, value = F)
+  measures <- grep( "="   ,data, value = F) # measures in .krn start wtih = 1
   measure_numbers <- 1:length(measures) # how many measures there are
-
-
   list_notes <- grep("^\\(*[0-9]|\\[|\\.", data) # makes a list of notes and . s
-
   val_list_notes <- grep("^\\(*[0-9]|\\[|\\.", data, value = T) # match ( maybe once,  [0-9 maybe once])
-
   measure_column <- vector() # make a vector of which row each is in.
   for( i in 2:length(measures)){
     len <- as.numeric(measures[i]-measures[(i-1)])-1
     val <- measure_numbers[i-1]
     measure_column <- c(measure_column, rep(val, len))
   }
-
-
   sep_chord <- list() #creates a list that seperates 4E 8n into two
-
-    for(i in 1:length(list_notes)){ # if not, seperate the notes
-      sep_chord[[i]] <- unlist(strsplit(val_list_notes[i],"\\/|\\\\"))
-    }
-
-
+  for(i in 1:length(list_notes)){ # if not, seperate the notes
+    sep_chord[[i]] <- unlist(strsplit(val_list_notes[i],"\\/|\\\\"))
+  }
   n <- data.frame() # seperates chords from list to data frame
   for( j in 1:3){
     for( i in 1:length(list_notes)){
@@ -52,13 +41,10 @@ kern2df <- function(spline){ # takes a spline(".krn") as input
 
   r1 <- str_extract(piece[,2],"[0-9]{1,2}(\\.*)")
   note_base <- str_extract(piece[,2],"[A-z]{1,2}.*")
-
   r2 <- str_extract(piece[,3],"[0-9]{1,2}(\\.*)")
   note_third <- str_extract(piece[,3],"[A-z]{1,2}.*")
-
   r3 <- str_extract(piece[,4],"[0-9]{1,2}(\\.*)")
   note_fifth <- str_extract(piece[,4],"[A-z]{1,2}.*")
-
 
   spline_df <- data.frame(measure = measure_column,
                       rhythm_val_base = r1,
@@ -72,10 +58,8 @@ kern2df <- function(spline){ # takes a spline(".krn") as input
 
 
 #######  piece_df #############
-
-
-
-#' Takes several splines in .krn that are part of a piece and calls kern2df to create one df for entire piece
+#' Takes several splines in .krn that are part of a piece 
+#' and calls kern2df to create one df for entire piece
 #'
 #' @param a first spline .krn file
 #' @param b second spline .krn file
@@ -83,21 +67,39 @@ kern2df <- function(spline){ # takes a spline(".krn") as input
 #' @return dataframe for entire piece - combined splines
 
 piece_df <- function(a,b,c){
-  df_a <- kern2df(a)
-  df_b <- kern2df(b)
-  df_c <- kern2df(c)
-  len <- nrow(df_a)
-  len_b <- nrow(df_b)
-  len_c <- nrow(df_c)
-#  if(len != len_b & len != len_c){ # make sure everything matches up
-
-  spline_number <- c(rep(1,len),rep(2,len),rep(3,len)) #indicates which spline everything belongs to
-
-  piece <- rbind(df_a,df_b,df_c)
-
-  piece <- cbind(spline_number, piece)
+# if want splines listed vertically
+#  len <- nrow(df_a)
+#  len_b <- nrow(df_b)
+#  len_c <- nrow(df_c)
+#  spline_number <- c(rep(1,len),rep(2,len),rep(3,len)) 
+#indicates which spline everything belongs to
+#  piece <- rbind(df_a,df_b,df_c)
+#  piece <- cbind(spline_number, piece)
+#  piece
+  A <- kern2df(a)
+  B <- kern2df(b)
+  C <- kern2df(c)
+  
+  measure_col <- A[,1]
+  A <- A[,-1] # dont need repeats of the measure number. Should be consistant. 
+  B <- B[,-1]
+  C <- C[,-1]
+    
+  piece <- cbind(measure_col,A,B,C) #combine splines vertically
+  
+  colnames(piece) <- c("measure","voice_r_val_1", "voice_n_val_1","voice_r_val_3",
+                       "voice_n_val_3","voice_r_val_5","voice_n_val_5","pR_r_val_1", 
+                       "pR_n_val_1","pR_r_val_3","pR_n_val_3",
+                       "pR_r_val_5","pR_n_val_5","pL_r_val_1", 
+                       "pL_n_val_1","pL_r_val_3",
+                       "pL_n_val_3","pL_r_val_5",
+                       "pL_n_val_5") # rename columns (voice, piano right hand, piano left hand)
+  tf <- 0
+  for(i in 1:ncol(piece)){   # Get rid of collumns with all NA's
+    tf[i] <- all(is.na(piece[,i]))
+  }
+  piece <- piece[,-which(as.logical(tf))]
   piece
-
 }
 
 ###all_info_piece
