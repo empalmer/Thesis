96bba5ab2580c4f16abc6eae60103eb5bdc77323
Emily Palmer <empalmer@reed.edu>
2018-04-21 19:50:14 -0700

    EDA

--- Models_EDA/EDA_bach_felix.R
+++ Models_EDA/EDA_bach_felix.R
@@ -12,8 +12,10 @@ pairs2 <- pairs(features[,-1])
 pairs2
 
 library(corrplot)
+setwd("~/Desktop/Thesis/Thesis_doc/images")
+pdf("cor_circles_b.pdf")
 corrplot(cor(features[,-1]), order = "hclust")
-
+dev.off()
 #==============================================================
 # Correlation matrix
 cor(features)
@@ -22,27 +24,34 @@ cor(features)
 # PCA and skree plots 
 
 PCA <- prcomp(features[,-1], scale = T)
-plot(PCA, type = "1", main="Principal Components Analysis")
 
 # biplot
+setwd("~/Desktop/Thesis/Thesis_doc/images")
+pdf("biplot_b.pdf")
 biplot(PCA,scale = 0,xlabs=rep("", nrow(PCA$x)))
+dev.off()
 
 # biplot with elipses and color groups, first two principal components
-g <- ggbiplot(PCA, obs.scale = 1, var.scale = 1, 
+pdf("bi_elipse12.pdf")
+ggbiplot(PCA, obs.scale = 1, var.scale = 1, 
               groups = features[,1], ellipse = TRUE, 
               circle = F)
-g
+dev.off()
 # biplot with elipses and color groups, first two principal components
-h <- ggbiplot(PCA, choices = 2:3,obs.scale = 1, var.scale = 1, 
+pdf("bi_elipse23.pdf")
+ggbiplot(PCA, choices = 2:3,obs.scale = 1, var.scale = 1, 
               groups = features[,1], ellipse = TRUE, 
               circle = F)
-h
+dev.off()
+
 #skreee
-d <- data.frame(PC = 1:13,
+d <- data.frame(PC = 1:21,
                 PVE = PCA$sdev^2 / sum(PCA$sdev^2))
+pdf("skree_b.pdf")
 ggplot(d, aes(x = PC, y = PVE)) +
   geom_line() + 
   geom_point()
+dev.off()
 
 # circle
 theta <- seq(0,2*pi,length.out = 100)
@@ -57,43 +66,42 @@ p + geom_text(data=loadings,
   labs(x = "PC1", y = "PC2")
 
 #==============================================================
-# distributions for all 
-
-
-a <- ggplot(features, aes(x = features$dens_mean,color = features[,1])) + 
-  geom_density()
-b <- ggplot(features, aes(x = features[,3],color = features[,1])) + 
-  geom_density()
-c <- ggplot(features, aes(x = features[,4],color = features[,1])) + 
-  geom_density()
-
-plot_grid(a,b)
+# Distributions for each features
+library(reshape)
+setwd("~/Desktop/Thesis/Thesis_doc/images")
+pdf("distribution_b.pdf")
+ggplot(melt(features), aes(x = value, color = composer)) + geom_density()+
+  facet_wrap(~variable, scales = "free")
+dev.off()
 
 #==============================================================
 # k-means clustering
 
+eleven_pcs <- PCA$x[,1:11]
 
-
-km2 <- kmeans(features[ ,-1], 2, nstart = 20)
-pca1 <- prcomp(features[, -1])
-d <- data.frame(PC1 = pca1$x[, 1],
-                PC2 = pca1$x[, 2],
+km2 <- kmeans(eleven_pcs, 2, nstart = 20)
+d <- data.frame(PC1 = PCA$x[, 1],
+                PC2 = PCA$x[, 2],
                 cluster = as.factor(km2$cluster),
                 state = features[,1])
+pdf("kmeans_2_b.pdf")
 ggplot(d, aes(x = PC1, y = PC2, col = cluster)) +
   geom_point() +
   geom_text(aes(label = state), vjust = 2)
+dev.off()
 
-km3 <- kmeans(features[ ,-1], 3, nstart = 20)
-pca1 <- prcomp(features[, -1])
-d <- data.frame(PC1 = pca1$x[, 1],
-                PC2 = pca1$x[, 2],
+#km3
+both <- c(features[1:37,1],ffeatures[,1])
+km3 <- kmeans(eleven_pcs, 3, nstart = 20)
+d <- data.frame(PC1 = PCA$x[, 1],
+                PC2 = PCA$x[, 2],
                 cluster = as.factor(km3$cluster),
                 state = features[,1])
+pdf("kmeans_3_b.pdf")
 ggplot(d, aes(x = PC1, y = PC2, col = cluster)) +
   geom_point() +
   geom_text(aes(label = state), vjust = 2)
-
+dev.off()
 
 
 
--- Models_EDA/EDA_fanny_felix.R
+++ Models_EDA/EDA_fanny_felix.R
@@ -15,8 +15,22 @@ pairs2 <- pairs(ffeatures[,-1])
 pairs2
 
 #==============================================================
-# Correlation matrix
+# Density distributions
+library(reshape)
+setwd("~/Desktop/Thesis/Thesis_doc/images")
+pdf("distribution_f.pdf")
+ggplot(melt(ffeatures), aes(x = value, color = fcomposer)) + geom_density()+
+  facet_wrap(~variable, scales = "free")
+dev.off()
+#==============================================================
+# Correlation matrix + visualization
 cor(ffeatures)
+
+library(corrplot)
+setwd("~/Desktop/Thesis/Thesis_doc/images")
+pdf("cor_circles_f.pdf")
+corrplot(cor(ffeatures[,-1]), order = "hclust")
+dev.off()
 
 #==============================================================
 # PCA and skree plots 
@@ -24,25 +38,27 @@ cor(ffeatures)
 PCA <- prcomp(ffeatures[,-1], scale = T)
 
 # biplot
+pdf("biplot_f.pdf")
 biplot(PCA,scale = 0, xlabs = rep("",nrow(PCA$x)))
-
+dev.off()
 
 # biplot with elipses and color groups, first two principal components
-g <- ggbiplot(PCA, obs.scale = 1, var.scale = 1, 
+pdf("bi_elipse_12_f.pdf")
+ggbiplot(PCA, obs.scale = 1, var.scale = 1, 
               groups = ffeatures[,1], ellipse = TRUE, 
               circle = F)
-g
+dev.off()
 # biplot with elipses and color groups, first two principal components
 h <- ggbiplot(PCA, choices = 2:3,obs.scale = 1, var.scale = 1, 
               groups = ffeatures[,1], ellipse = TRUE, 
               circle = F)
 h
 #skreee
-d <- data.frame(PC = 1:13,
+d <- data.frame(PC = 1:21,
                 PVE = PCA$sdev^2 / sum(PCA$sdev^2))
-ggplot(d, aes(x = PC, y = PVE)) +
-  geom_line() + 
-  geom_point()
+pdf("skree_f.pdf")
+ggplot(d, aes(x = PC, y = PVE)) + geom_line() + geom_point()
+dev.off()
 
 # circle
 theta <- seq(0,2*pi,length.out = 100)
@@ -60,12 +76,15 @@ p + geom_text(data=loadings,
 #===================================================
 # clustering
 
-km2 <- kmeans(ffeatures[ ,-1], 2, nstart = 20)
-pca1 <- prcomp(ffeatures[, -1],scale = T)
-d <- data.frame(PC1 = pca1$x[, 1],
-                PC2 = pca1$x[, 2],
+df <- PCA$x[,1:11]
+
+km2 <- kmeans(df, 2, nstart = 20)
+d <- data.frame(PC1 = PCA$x[, 1],
+                PC2 = PCA$x[, 2],
                 cluster = as.factor(km2$cluster),
                 state = ffeatures[,1])
+pdf("kmeans_2_f.pdf")
 ggplot(d, aes(x = PC1, y = PC2, col = cluster)) +
   geom_point() +
   geom_text(aes(label = state), vjust = 2)
+dev.off()
